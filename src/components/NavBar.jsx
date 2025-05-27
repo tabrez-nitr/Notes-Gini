2a83a8bc553aa63efcfa945a112c47bd47419eeb
tabrez <04tabrez@gmail.com>
2025-05-27 19:29:52 +0530

    style: interface change of homepage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,18 +5,24 @@ import { useAuth } from '../context/AuthContext'
 
 function NavBar() {
 
-  const {user , signOutUser} = useAuth();
+  const {user , signOutUser , signIn} = useAuth();
 
   return (
-    <div className='bg-[#021526] h-[9vh] flex justify-between p-4 border-b-1 border-white/50'>
+    <div className='bg-white h-[7vh] flex justify-between p-4 border-b-1 text-black border-black/30'>
       
-         <Link to=""><img src={logo} alt="" className='w-[55px] h-[55px] ml-4 rounded-4xl border-1 border-white' /></Link>
-         <div className='flex gap-10 pt-2 mr-10'>
-         <Link to="" className='text-2xl text-white hover:text-[#4796E3]'>Home</Link>
-         <Link to="note" className='text-2xl text-white hover:text-[#4796E3]'>Note</Link>
+         <ul className='flex items-center'><Link className='font-medium text-2xl'>Notes <span className='text-3xl'
+          style={{
+            fontFamily :'Brush Script MT',
+          }}
+         >Gini</span></Link></ul>
+         <div className='flex gap-10  mr-10'>
+         <ul className='flex items-center'><Link to="" className='  hover:text-[#4796E3]'>Home</Link></ul>
+          <ul className='flex items-center'><Link  className=' hover:text-[#4796E3]'>Features</Link></ul>
+         <ul className='flex items-center'><Link to="note" className=' hover:text-[#4796E3]'>Note</Link></ul>
+         <ul className='flex items-center'><button type='button'  className='font-medium cursor-pointer bg-[#DDE7F2] p-2  px-5 rounded-[8px] hover:bg-[#afcbe6]' onClick={()=> signIn()}>Get Started</button></ul>
          </div>
            
-        {user !== null ?  <button className='text-[20px] text-white hover:text-red-400 p-2 cursor-pointer ' 
+        {user !== null ?  <button className='text-[20px]  hover:text-red-400 p-2 cursor-pointer ' 
         onClick={()=> signOutUser()}
         >Sign Out</button>: ""}
                
